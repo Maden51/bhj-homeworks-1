e1c5c7e2e68fdcf66f8a2c522af5f721a7172b29
Maden5126 <madenon123@gmail.com>
2021-07-26 11:41:17 +0300

    2 and 3 tasks added

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,10 +1,7 @@
 const count = document.getElementById("clicker__counter");
-count.onclick = function() {
-    count++;
-};
-
 const image = document.getElementById("cookie");
 function changeSizes() {
+    count.textContent++;
     if (image.width === 200) {
         image.width = 150;
     } else if (image.width === 150) {
