75b96eea2fccd0ad185e5bb03df276157ba07c7d
Maden5126 <madenon123@gmail.com>
2021-07-27 12:24:38 +0300

    updated first task

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -2,11 +2,7 @@ const count = document.getElementById("clicker__counter");
 const image = document.getElementById("cookie");
 function changeSizes() {
     count.textContent++;
-    if (image.width === 200) {
-        image.width = 150;
-    } else if (image.width === 150) {
-        image.width = 200;
-    }
+    image.width = image.width === 200 ? 250 : 200;
 };
 
 image.onclick = changeSizes;
