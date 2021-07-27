3e0a19fcfc86c31a6cccedfc24261ba96b68155f
Maden5126 <madenon123@gmail.com>
2021-07-27 12:28:49 +0300

    updated second taks

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -2,5 +2,5 @@ const countdown = function() {
     const count = document.getElementById("timer");
     count.textContent--;
 }
-setInterval(countdown, 1000);
-setTimeout(() => { clearInterval(countdown); alert('«Вы победили в конкурсе!»'); }, 60000);
+let timerId = setInterval(countdown, 1000);
+setTimeout(() => { clearInterval(timerId); alert('«Вы победили в конкурсе!»'); }, 59000);
