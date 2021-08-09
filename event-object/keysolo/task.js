8b92d9f7cd579d8169f24b89c02a68450b357d70
Maden5126 <madenon123@gmail.com>
2021-08-09 15:53:47 +0300

    task 2 added

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,13 +17,15 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
+    document.addEventListener('keydown', (event) => {
+      let currentSymbolUp = this.currentSymbol.textContent.toUpperCase();
+      let keydownSymbolUp = event.key.toUpperCase();
+      if (currentSymbolUp === keydownSymbolUp) {
+        this.success();
+      } else {
+        this.fail();
+      }
+    });
   }
 
   success() {
