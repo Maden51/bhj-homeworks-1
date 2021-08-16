d8d1d84315e96faba90d74714419c404e98307f7
Maden5126 <madenon123@gmail.com>
2021-08-16 10:21:18 +0300

    task 1 updated

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -2,7 +2,7 @@ const phrase = Array.from(document.querySelectorAll('.reveal'))
 
 window.addEventListener('scroll', function(){
     phrase.forEach(e => {
-        e.getBoundingClientRect().top > 0 && e.getBoundingClientRect().top < window.innerHeight ?
-        e.classList.add('reveal_active') : e.classList.remove('reveal_active');
+        const top = e.getBoundingClientRect().top;
+        top > 0 && top < window.innerHeight ? e.classList.add('reveal_active') : e.classList.remove('reveal_active');
     })
 });
