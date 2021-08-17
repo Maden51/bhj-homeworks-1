a6b7a8eefce123df29a76b66b57eda0de3a58ec0
Maden5126 <madenon123@gmail.com>
2021-08-17 15:49:37 +0300

    task 1 added

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,22 @@
+const elemsWithToolTip = document.querySelectorAll('.has-tooltip');
+const toolTip = document.createElement('div');
+const actToolTip = document.querySelector('.tooltip_active');
+toolTip.classList.add('tooltip');
+
+elemsWithToolTip.forEach((item) => {
+    item.addEventListener('click', e => {
+        e.preventDefault();
+        item.appendChild(toolTip);
+
+        let elemTopCoords = e.target.getBoundingClientRect().top;
+        let elemLeftCoords = e.target.getBoundingClientRect().left;
+        let textToolTip = item.getAttribute('title');
+
+        toolTip.contains(actToolTip) && actToolTip.innerText == e.target.title ? 
+        toolTip.classList.remove('tooltip_active') : toolTip.classList.add('tooltip_active');
+
+        toolTip.style.top = elemTopCoords + 20 + 'px';
+        toolTip.style.left = elemLeftCoords + 'px';
+        toolTip.innerText = textToolTip;
+    });
+});
