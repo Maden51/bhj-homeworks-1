d4f1f2d0bba4b933bc3e0fdc49b3c4a8609fa9d2
Maden5126 <madenon123@gmail.com>
2021-08-19 11:06:25 +0300

    homework updated

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -2,16 +2,17 @@ const rotator = Array.from(document.querySelectorAll('.rotator__case'));
 let delay = 1000;
 let index = 0;
 
-setInterval(function() {
+function rotate() {
     rotator.forEach((item) => item.classList.remove('rotator__case_active'));
-
-    rotator[index].classList.add('rotator__case_active');
     rotator[index].style.color = rotator[index].dataset.color;
-
+    rotator[index].classList.add('rotator__case_active');
+    
     delay = rotator[index].dataset.speed;
-    console.log(delay);
+    setTimeout(rotate, delay);
 
     if (++index >= rotator.length) {
         index = 0;
     }
-}, delay);
+};
+
+setTimeout(rotate, delay);
